7f38fc01f9e042214b75c98afbb42d829085991e
Jane <jkrytsyna@gmail.com>
2026-05-13 12:15:47 -0700

    refactoring

---
+++ app/src/main/kotlin/com/itclimb/spellcoach/core/designsystem/tokens/AppBorder.kt
@@ -0,0 +1,8 @@
+package com.itclimb.spellcoach.core.designsystem.tokens
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+object AppBorder {
+    val hairline: Dp = 1.dp
+}
