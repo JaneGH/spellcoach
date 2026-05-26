f59145461c5c413292c078716d37064537583f36
Jane <jkrytsyna@gmail.com>
2026-05-26 10:34:26 -0700

    changed compileSdk and targetSdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.itclimb.spellcoach"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.itclimb.spellcoach"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
